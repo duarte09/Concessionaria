eb53c2ae4aa1bb61b449524782341b34a6448aaa
marcusmarchiori <marcus_marchiori@hotmail.com>
2024-11-08 00:09:09 -0300

    Alteração dados sql

--- sql-tabelas.sql
+++ sql-tabelas.sql
@@ -2,33 +2,46 @@ CREATE DATABASE autoficticio;
 
 USE autoficticio;
 
-CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE anunciante (
+    id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     cpf VARCHAR(11) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    senha VARCHAR(255) NOT NULL,
+    senhahash VARCHAR(255) NOT NULL,
     telefone VARCHAR(15) NOT NULL
 );
 
-CREATE TABLE veiculos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE anuncio (
+    id INT PRIMARY KEY AUTO_INCREMENT,
     usuario_id INT,
     marca VARCHAR(50),
     modelo VARCHAR(50),
+    ano INT NOT NULL,
     cor VARCHAR(20),
     quilometragem INT,
     descricao TEXT,
     localizacao VARCHAR(50),
     valor DECIMAL(10, 2),
-    imagem VARCHAR(255),
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
+    datahora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    estado VARCHAR(2) NOT NULL,
+    cidade VARCHAR(100) NOT NULL,
+    idanunciante INT NOT NULL,
+    FOREIGN KEY (idanunciante) REFERENCES anunciante(id) ON DELETE CASCADE
 );
 
-CREATE TABLE interesses (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    usuario_id INT,
-    veiculo_id INT,
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
-    FOREIGN KEY (veiculo_id) REFERENCES veiculos(id)
+CREATE TABLE interesse (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    telefone VARCHAR(15),
+    mensagem TEXT,
+    datahora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    idanuncio INT NOT NULL,
+    FOREIGN KEY (idanuncio) REFERENCES anuncio(id)
+);
+
+CREATE TABLE foto (
+    idanuncio INT NOT NULL,
+    nomearqfoto VARCHAR(255) NOT NULL,
+    PRIMARY KEY (idanuncio, nomearqfoto),
+    FOREIGN KEY (idanuncio) REFERENCES anuncio(id)
 );
